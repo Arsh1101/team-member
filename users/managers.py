4c5a1af083d7e4f9fa74e47e2766898131b58c11
arsh <arshia.alidosti.72@gmail.com>
2023-02-03 21:59:28 -0800

    Add the model and manager to create custom user model.

---
+++ users/managers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager.
+    Email is the unique identifier.
+    """
+    def create_user(self, email, password, first_name, last_name , phone_number, team_role, **extra_fields):
+        """
+        Create and save a user with the given infromation.
+        """
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name , phone_number=phone_number, team_role= team_role, **extra_fields)
+        #Temprorary value, brfore create a send a random password service.
+        password = "temp"
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given infromation.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        #Validations
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        #Internal users (super users) can fill these parameters later.
+        #Super users are not team admins in the definition of the team.
+        #I used None, to add defult value for admins.
+        first_name, last_name , phone_number, team_role = "", "", "", None
+        return self.create_user(email, password, first_name, last_name , phone_number, team_role, **extra_fields)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    # I assumed first_name, last_name, email , phone_number, and team_roll should not be blank.
+    # If you want to be optional fields, I can change them.
+    email = models.EmailField("Email address", unique=True)
+    first_name = models.CharField("First name", max_length=150)
+    last_name = models.CharField("Last name", max_length=150)
+    # Phone numbers could be unique too, but for now, is not.
+    phone_number = models.PhoneNumberField("Phone number", null=False, blank=False)
+    # I decided to use choices for team roles.
+    # The other option was the Boolean field.
+    # I think 'choices' is a better approach.
+    # In the future, if we want to add different roles, it is easier to extend the functionality.
+    # Seperation with use of variables for error prevention and logic separation.
+    # For more complex roles in future we can have a differente table or use django roles.
+    ADMIN = "Admin"
+    REGULAR = "Regular"
+    TEAM_ROLLS = [ 
+        (ADMIN, "Can delete members"),
+        (REGULAR, "Can't delete members"),
+        ]
+    team_roll = models.CharField("Team Roll",
+        max_length=3,
+        choices=TEAM_ROLLS,
+        default=REGULAR,
+    )
+    #Super user properties.
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    #For future data managment is good to have this field. 
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [ email, first_name, last_name , phone_number ]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
